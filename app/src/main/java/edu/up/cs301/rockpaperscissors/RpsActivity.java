1819615e614e984daeecef5b71220791a884018b
vegdahl <vegdahl@up.edu>
2016-09-25 23:26:14 -0700

    Initial commit.

---
+++ app/src/main/java/edu/up/cs301/rockpaperscissors/RpsActivity.java
@@ -0,0 +1,51 @@
+package edu.up.cs301.rockpaperscissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import edu.up.cs301.animation.AnimationSurface;
+import edu.up.cs301.animation.Animator;
+
+public class RpsActivity extends AppCompatActivity {
+
+    // the animator
+    private RpsAnimator animator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rps);
+
+
+        // Connect the animation surface with the animator
+        AnimationSurface mySurface = (AnimationSurface) this.findViewById(R.id.animationSurface);
+        animator = new RpsAnimator();
+        mySurface.setAnimator(animator);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_rps, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
